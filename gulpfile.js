ec93e5eb9fda6181bb95d1bf03c035109f8241a8
Asa Smith <smith.asa.la@gmail.com>
2018-01-03 19:33:26 -0500

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('*.js', browserSync.reload)
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+    port: 1111,
+    open: false
+  })
+})
