96ed2c213d96c3a8b06fbbf2521d3b130a12db59
Asa Smith <smith.asa.la@gmail.com>
2018-01-03 22:16:46 -0500

    make todos objects

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 console.log('hello world')
+
+// todoList.addTodo should add objects
+// // todoList.changeTodo should change the todoText property
+// // todoList.toggleCompleted should change the completed property
+//
+const todoList = {
+   
+todos: [],
+  display: function() {
+    console.log('Todos:')
+    for(let i = 0; i < this.todos.length; i++) {
+      console.log(this.todos[i].todoText, this.todos[i].completed)
+        }
+  },
+  add: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    })
+    this.display()
+  },
+  edit: function(position, todoText) {
+    let index = position - 1 // gets actual array index
+    this.todos[index].todoText = todoText
+    this.display()
+  },
+  toggleCompleted: function(position) {
+    let index = position - 1 //gets actual array index 
+    let todo = this.todos[index]
+    todo.completed = !todo.completed
+    this.display()
+  },
+  delete: function(position) {
+    let index = position -1 // gets actual array index
+    this.todos.splice(index, 1)
+    this.display()
+  }
+}
