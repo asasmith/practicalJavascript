49349f413b3503cc970bfdeece642dd5ea90e017
Asa Smith <smith.asa.la@gmail.com>
2018-02-06 18:03:52 -0500

    test todoList.delete

--- test/todoListTest.js
+++ test/todoListTest.js
@@ -13,11 +13,23 @@ test('todoList.todoList.add returns a function', t => {
 
 // test add function
 test('todoList.add("test") adds a todo list item', t => {
-  t.deepEqual(todoList.todoList.add('test'), [{todoText: 'test', isComplete: false}])
+  t.deepEqual(todoList.todoList.add('test one'), [{todoText: 'test one', isComplete: false}])
   t.log(todoList.todoList.todos)
 })
 
-// test delet function
+// add onther todo item
+test('todoList.add("test") adds a todo list item', t => {
+  t.deepEqual(todoList.todoList.add('test two'), [{todoText: 'test one', isComplete: false}, {todoText: 'test two', isComplete: false}])
+  t.log(todoList.todoList.todos)
+})
+
+// test delete function
+test('todoList.delete(1) should delete the second todo item', t => {
+  t.deepEqual(todoList.todoList.delete(1), [{todoText: 'test one', isComplete: false}])
+  t.log(todoList.todoList.todos)
+})
+
+// test delete function
 test('todoList.delete(0) should delete the first todo item', t => {
   t.deepEqual(todoList.todoList.delete(0), [])
   t.log(todoList.todoList.todos)
