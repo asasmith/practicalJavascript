cca55c5712356723722230ded0320dddfd72c347
Asa Smith <smith.asa.la@gmail.com>
2018-02-07 16:57:54 -0500

    add tests for todoList.edit()

--- test/todoListTest.js
+++ test/todoListTest.js
@@ -1,36 +1,46 @@
 import test from 'ava'
-import todoList from '../src/todoList'
+import { todoList } from '../src/todoList'
 
 // check that todoList is an object
-test('todoList.todoList returns an object', t => {
-  t.true(typeof todoList.todoList === 'object')
+test('todoList returns an object', t => {
+  t.true(typeof todoList === 'object')
 })
 
 // check that todoList.add is a function
-test('todoList.todoList.add returns a function', t => {
-  t.true(typeof todoList.todoList.add === 'function')
+test('todoList.add returns a function', t => {
+  t.true(typeof todoList.add === 'function')
 })
 
-// test add function
+// // test add function
 test('todoList.add("test") adds a todo list item', t => {
-  t.deepEqual(todoList.todoList.add('test one'), [{todoText: 'test one', isComplete: false}])
-  t.log(todoList.todoList.todos)
+  t.deepEqual(todoList.add('test one'), [{todoText: 'test one', isComplete: false}])
 })
 
-// add onther todo item
+// add another todo item
 test('todoList.add("test") adds a todo list item', t => {
-  t.deepEqual(todoList.todoList.add('test two'), [{todoText: 'test one', isComplete: false}, {todoText: 'test two', isComplete: false}])
-  t.log(todoList.todoList.todos)
+  t.deepEqual(todoList.add('test two'), [{todoText: 'test one', isComplete: false}, {todoText: 'test two', isComplete: false}])
+})
+
+// todoList.edit is a function
+test('todoList.edit() is a function', t => {
+  t.true(typeof todoList.edit === 'function')
+})
+
+// edit todos
+test('todoList.edit(0, "edited") edits the first todo item', t => {
+  t.deepEqual(todoList.edit(0, 'edited'), [{todoText: 'edited', isComplete: false}, {todoText: 'test two', isComplete: false}])
+})
+
+test('todoList(0, "edited") edits the second todo item', t => {
+  t.deepEqual(todoList.edit(1, 'edited too'), [{todoText: 'edited', isComplete: false}, {todoText: 'edited too', isComplete: false}])
 })
 
 // test delete function
 test('todoList.delete(1) should delete the second todo item', t => {
-  t.deepEqual(todoList.todoList.delete(1), [{todoText: 'test one', isComplete: false}])
-  t.log(todoList.todoList.todos)
+  t.deepEqual(todoList.delete(1), [{todoText: 'edited', isComplete: false}])
 })
 
 // test delete function
 test('todoList.delete(0) should delete the first todo item', t => {
-  t.deepEqual(todoList.todoList.delete(0), [])
-  t.log(todoList.todoList.todos)
+  t.deepEqual(todoList.delete(0), [])
 })
